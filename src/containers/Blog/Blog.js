fe0e1d1e486657f55719de04cf7ed0f523425574
Mukesh Kumar Shahi <mukesh.shahi@telusinternational.com>
2020-03-12 19:48:16 +0530

    fitrst

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import axios from "axios";
+class Blog extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 3);
+      this.setState({ posts: posts });
+    });
+  }
+
+  render() {
+    const posts = this.state.posts.map((posts) => {
+      return <Post key={posts.id} title={posts.title}></Post>;
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
